a8fcc3ce3ca8e499cde609b3a91fb0f38e1007fe
zeus-iitg <vaibhavsingh878@gmail.com>
2021-09-17 14:33:40 +0530

    changed server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/paper-kit-2-angular'));
+app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/paper-kit-2-angular/'}),
+    res.sendFile('index.html', {root: 'dist/'}),
 );
 
 // Start the app by listening on the default Heroku port
